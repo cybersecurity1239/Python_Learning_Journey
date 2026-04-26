023f266794d7c775fed90ae41191756f43dff517
cybersecurity1239 <154923797+cybersecurity1239@users.noreply.github.com>
2026-04-26 13:03:12 +0000

    Listeler , float hesaplamaları ve yorum satırı denemeleri yapıldı.

---
+++ 01Degıskenler.py
@@ -0,0 +1,19 @@
+x = 5
+y = 4
+x *y 
+print(x*y)
+x*(x + y)
+print(x*(x + y))
+type (x*(x + y))
+print(type(x*(x + y)))
+y = 4.0 
+print(type(x*(x + y)))
+########
+ulkeler = ["Türkiye","İtalya","Brazilya"]
+print(ulkeler)
+# Ülkelerin diplomatik güç listesi 
+# Float kullanarak hassas ölçüm yapıyoruz
+gucler = [0.85, 0.42, 0.91] 
+#En güçlü ülkeyi seç ve ekrana bas
+print(" En yüksek diplomatik güç: ") 
+print (gucler [2])
